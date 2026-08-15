0b474716597be91cd34d79833c83e6d3935971ab
dev04-ux <dev396402@gmail.com>
2026-08-15 23:38:25 +0530

    Add files via upload

--- Day6.c
+++ Day6.c
@@ -1,16 +1,19 @@
-
 #include<stdio.h>
 int main(){
     int a;
     printf("A:");
     scanf("%d",&a);
 
-if(a%2==0){
-    printf(" %d IS EVEN .\n",a);
-}
-else{
-    printf(" %d IS ODD.\n",a);
-}
+    if(a>0){
+        printf("POSITIVE");
+    }
+    else if(a<0){
+        printf("NEGATIVE");
+    }
+    else
+    {
+        printf("ZERO");
+    }
 
 
 }
