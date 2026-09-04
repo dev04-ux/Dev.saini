31cfae8c3cf608c2a5067501f0dd136aaba327a4
Dev Saini <dev396402@gmail.com>
2026-09-04 23:17:42 +0530

    Add files via upload

--- Day26.c
+++ Day26.c
@@ -1,12 +1,20 @@
-int main() {   
- int i, j, s;  
-  for (i = 5; i >= 1; i--) {
- for (s = 1; s < i; s++) {        
- printf(" ");     
- }        
- for (j = i; j <= 5; j++) {
-    printf("%d", j);     
-}        printf("\n");    
-}      
-  return 0;
+
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+        int stars = (i <= (n + 1) / 2) ? (2 * i - 1) : (2 * (n - i + 1) - 1);
+        
+        for (j = 1; j <= stars; j++) {
+            printf("*\n");
+        }
+        if (i < n) {
+            printf("\n");
+        }
+    }
+
+    return 0;
 }
