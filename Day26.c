899d6289ca0682484bcfc18f6f06c1deb98359e8
Dev Saini <dev396402@gmail.com>
2026-09-04 23:16:15 +0530

    Add files via upload

---
+++ Day26.c
@@ -0,0 +1,12 @@
+int main() {   
+ int i, j, s;  
+  for (i = 5; i >= 1; i--) {
+ for (s = 1; s < i; s++) {        
+ printf(" ");     
+ }        
+ for (j = i; j <= 5; j++) {
+    printf("%d", j);     
+}        printf("\n");    
+}      
+  return 0;
+}
