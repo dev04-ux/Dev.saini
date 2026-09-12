30f689f741eea5cd8829cf04af4940a7b4aa251d
Dev Saini <dev396402@gmail.com>
2026-09-12 22:06:54 +0530

    Add files via upload

---
+++ Day34.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, pos, element;
+    int arr[100];
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d %d", &pos, &element);
+
+    // Shift elements to the right
+    for (i = n; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos] = element;
+    n++;
+
+    // Print the updated array
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
