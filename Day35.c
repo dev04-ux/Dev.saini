e6398c09c0f25749df12a4f9b6a94e714ff28418
Dev Saini <dev396402@gmail.com>
2026-09-13 22:58:14 +0530

    Add files via upload

--- Day35.c
+++ Day35.c
@@ -1,36 +1,50 @@
 
 #include <stdio.h>
-#include <limits.h>
- 
-int main()
-{
-	int arr[50], i, Size;
-	int first, second;
-	
-	printf("\n Please Enter the Number of elements in an array  :  ");
-	scanf("%d", &Size);
-	
-	printf("\n Please Enter %d elements of an Array \n", Size);
-	for (i = 0; i < Size; i++)
-	{
-		scanf("%d", &arr[i]);
+
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
     }
-	 
-	first = second = INT_MIN;  
-	   
-	for (i = 0; i < Size; i++)
-	{
-		if(arr[i] > first)
-		{
-			second = first;
-			first = arr[i];
-		}
-		else if(arr[i] > second && arr[i] < first)
-		{
-			second = arr[i];
-		}	
-	}
-	printf("\n The Second Largest Number in this Array =  %d", second);
-	
-	return 0;
+}
+
+void rotateRight(int arr[], int n, int k) {
+    k = k % n; 
+
+    if (k <= 0) return;
+    reverse(arr, 0, n - 1);
+    reverse(arr, 0, k - 1);
+    reverse(arr, k, n - 1);
+}
+
+int main() {
+    int n, k;
+
+    printf("Enter the size of the array: ");
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("Invalid array size.\n");
+        return 1;
+    }
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the number of positions to rotate (k): ");
+    scanf("%d", &k);
+
+    rotateRight(arr, n, k);
+    printf("Rotated array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
